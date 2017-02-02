b5a6582778fe29bec9334e7c7ab6ef8595ff058a
Keylost <roman1@tambo.ru>
2017-02-02 19:13:58 +0700

    [-] *

--- sources/server.cpp
+++ sources/server.cpp
@@ -224,14 +224,10 @@ void* server_fnc(void *ptr)
 	while(1)
 	{
 			curObj = queue.waitForNewObject(curObj);
-			//printf("img\n");
 			curLineData = qline.waitForNewObject(curLineData);
-			//printf("line\n");
 			Mat &frame = *(curObj->obj);
 			imencode(".jpg", frame, buffer, parameters);
-			//printf("encode\n");
 			syst.engine_get(eng);
-			//printf("engine\n");
 			
 			srv.send(Image_t,(uint32_t)buffer.size(),(void *)(&buffer[0]));
 			srv.send(Line_t,sizeof(line_data),(void *)(curLineData->obj));
@@ -243,11 +239,9 @@ void* server_fnc(void *ptr)
 			{
 				srv.send(Sing_t,sizeof(sign_data),(void *)(&Signs[i]));
 			}
-			//printf("send\n");
 			
 			curObj->free();
 			curLineData->free();
-			//printf("data free\n");
 	}
 	
 	return NULL;
